655519e44bbe6e1b33e7e7731132b2bd55fd05ba
skwyz <abdelkarim.bengrine@epitech.eu>
2024-03-12 15:44:24 +0000

    add: getUserId

--- sources/token.go
+++ sources/token.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,33 @@ import (
 	"os"
 	"strings"
 )
+
+func getUserId(c *gin.Context) (int32, error) {
+
+	bearerToken := c.Request.Header.Get("Authorization")
+	tokenString := ""
+
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		tokenString = strings.Split(bearerToken, " ")[1]
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid token format"})
+	}
+
+	tokenPure, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("secret_key")), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	if claims, ok := tokenPure.Claims.(jwt.MapClaims); ok && tokenPure.Valid {
+		userID, _ := claims["user_id"].(float64)
+		return int32(userID), nil
+	} else {
+		return 0, errors.New("invalid token")
+	}
+}
 
 func JwtAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
